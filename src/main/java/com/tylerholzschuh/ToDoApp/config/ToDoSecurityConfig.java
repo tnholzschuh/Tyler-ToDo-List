3d508d27090680d3f05703481d8e22ef4af487ee
Tyler Holzschuh <tnholzschuh@gmail.com>
2020-12-18 19:27:25 -0800

    Initial commit, in memory BCrypt authentication

---
+++ src/main/java/com/tylerholzschuh/ToDoApp/config/ToDoSecurityConfig.java
@@ -0,0 +1,46 @@
+package com.tylerholzschuh.ToDoApp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class ToDoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated()
+                .and()
+                .formLogin();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("tyler")
+                .passwordEncoder(passwordEncoder()::encode)
+                .password("hello")
+                .roles("USER")
+                .build();
+
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+
+        userDetailsManager.createUser(user);
+
+        return userDetailsManager;
+    }
+}
---
+++ src/main/java/com/tylerholzschuh/ToDoApp/config/ToDoSecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.tylerholzschuh.ToDoApp.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class ToDoSecurityWebApplicationInitializer
+						extends AbstractSecurityWebApplicationInitializer {
+
+}
