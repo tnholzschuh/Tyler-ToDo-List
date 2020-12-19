06c8534df0f0bc220057202ef49a2d1abae5adae
Tyler Holzschuh <tnholzschuh@gmail.com>
2020-12-18 20:10:26 -0800

    Added view to show in memory to-do list

--- src/main/java/com/tylerholzschuh/ToDoApp/controllers/MainController.java
+++ src/main/java/com/tylerholzschuh/ToDoApp/controllers/MainController.java
@@ -1,14 +1,24 @@
 package com.tylerholzschuh.ToDoApp.controllers;
 
+import com.tylerholzschuh.ToDoApp.services.ToDoItemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
+    private final ToDoItemService toDoItemService;
+
+    @Autowired
+    public MainController(ToDoItemService toDoItemService) {
+        this.toDoItemService = toDoItemService;
+    }
+
     @GetMapping("/to-do-items")
-    public String toDoItems() {
-//        System.out.println(user);
+    public String toDoItems(Model model) {
+        model.addAttribute("toDoItems", toDoItemService.getToDoItemsByUsername("hello"));
         return "to-do-items";
     }
 
